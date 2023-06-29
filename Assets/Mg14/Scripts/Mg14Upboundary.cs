6a4c395da1991b83fc00e5baf9a39aed445888e0
heinheg <80655554+heinheg@users.noreply.github.com>
2023-06-29 14:53:43 +0900

    Mg14 원숭이 나무 프로토타입

---
+++ Assets/Mg14/Scripts/Mg14Upboundary.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mg14Upboundary : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<Mg14Player>().GetHit();
+        }
+
+    }
+}
---
+++ Assets/Mg14/Scripts/Mg14jellySpawner.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mg14jellySpawner : MonoBehaviour
+{
+    public GameObject jelly;
+
+
+
+
+
+    [SerializeField]
+    private float time_diff = 1.5f;
+
+    float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if (time > time_diff)
+        {
+            GameObject new_jelly = Instantiate(jelly);
+
+            // 좌표를 랜덤하게 선택하여 설정
+            Vector2 spawnPosition = new Vector2(Random.Range(-3.0f, 3.0f), 5f);
+            new_jelly.transform.position = spawnPosition;
+
+
+
+            time = 0;
+            Destroy(new_jelly, 5.0f);
+        }
+    }
+
+
+
+
+    public void IncreaseSpeed()
+    {
+        time_diff -= 0.1f; // 젤리의 생성 간격 감소
+    }
+
+
+}
