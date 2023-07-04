f4c76a553c7c1b35c59e813eaf5d2b834fb7878c
heinheg <80655554+heinheg@users.noreply.github.com>
2023-07-04 15:36:40 +0900

    Mg5버튼/에니메이션 추가

--- Assets/Scripts/MiniGame/Mg5/Mg5Player.cs
+++ Assets/Scripts/MiniGame/Mg5/Mg5Player.cs
@@ -5,68 +5,80 @@ using UnityEngine.UI;
 
 public class Mg5Player : MonoBehaviour
 {
-    // Start is called before the first frame update
     Rigidbody2D rigidbody2D;
+    Animator animator;
 
     [SerializeField]
     private float moveSpeed = 5f;
-    // Start is called before the first frame update
-    public int level;
 
     private bool RightButton = false;
     private bool LeftButton = false;
 
 
+    
 
-    public Mg5Player()
-    {
-        level = 1;
-    }
-
-    void Start()
+    private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
-
+        animator = GetComponent<Animator>();
     }
 
     public void RightClick()
     {
         LeftButton = false;
         RightButton = true;
+        
     }
 
     public void RightClickOff()
     {
         RightButton = false;
+        
     }
 
     public void LeftClick()
     {
         RightButton = false;
         LeftButton = true;
+        
     }
+
     public void LeftClickOff()
     {
         LeftButton = false;
+        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (Input.GetKey(KeyCode.LeftArrow) || LeftButton)
         {
-            Vector3 currScale = transform.localScale;
-            transform.localScale = new Vector3(-Mathf.Abs(currScale.x), currScale.y, currScale.z);
-            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+            animator.SetBool("Quokkastand", false);
+            animator.SetBool("QuokkaRightRun", false);
+            animator.SetBool("QuokkaLeftRun", true);
 
+            Vector3 currScale = transform.localScale;
+            transform.localScale = new Vector3(Mathf.Abs(currScale.x), currScale.y, currScale.z);
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.RightArrow) || RightButton)
+
         {
+            animator.SetBool("Quokkastand", false);
+            animator.SetBool("QuokkaLeftRun", false);
+            animator.SetBool("QuokkaRightRun", true);
             Vector3 currScale = transform.localScale;
             transform.localScale = new Vector3(Mathf.Abs(currScale.x), currScale.y, currScale.z);
             transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-
         }
-
+        else
+        {
+            animator.SetBool("QuokkaLeftRun", false);
+            animator.SetBool("QuokkaRightRun", false);
+            animator.SetBool("Quokkastand", true);
+        }
+        
+       
     }
+
 }
--- Assets/Scripts/MiniGame/Mg5/ObstacleSpawner.cs
+++ Assets/Scripts/MiniGame/Mg5/ObstacleSpawner.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    public GameObject Mg5obstacle;
+    public GameObject Mg5Obstacle;
     [SerializeField]
-    public int minNumMg5obstacleToSpawn = 1; // 최소 생성 장애물 개수
+    public int minNumMg5ObstacleToSpawn = 3; // 최소 생성 장애물 개수
     [SerializeField]
-    public int maxNumMg5obstacleToSpawn = 3; // 최대 생성 장애물 개수
+    public int maxNumMg5ObstacleToSpawn = 5; // 최대 생성 장애물 개수
 
     [SerializeField]
-    private float Mg5obstacleSpeed = 2.0f; // 장애물의 초기 속도
+    private float Mg5ObstacleSpeed = 4.0f; // 장애물의 초기 속도
 
     [SerializeField]
-    public float minTimeDiff = 3.0f; // 최소 생성 간격
+    public float minTimeDiff = 2.0f; // 최소 생성 간격
     [SerializeField]
-    public float maxTimeDiff = 5.0f; // 최대 생성 간격
+    public float maxTimeDiff = 3.0f; // 최대 생성 간격
 
 
     float time = 0;
@@ -34,16 +34,16 @@ public class ObstacleSpawner : MonoBehaviour
         time += Time.deltaTime;
         if (time > timeDiff)
         {
-            int numMg5obstacleToSpawn = Random.Range(minNumMg5obstacleToSpawn, maxNumMg5obstacleToSpawn + 1);
+            int numMg5obstacleToSpawn = Random.Range(minNumMg5ObstacleToSpawn, maxNumMg5ObstacleToSpawn + 1);
 
             for (int i = 0; i < numMg5obstacleToSpawn; i++)
             {
-                GameObject new_Mg5obstacle = Instantiate(Mg5obstacle);
-                float posX = Random.Range(-3f, 7.85f); // x 좌표를 랜덤으로 설정
-                new_Mg5obstacle.transform.position = new Vector3(posX,-5, 0);
-                new_Mg5obstacle.GetComponent<Mg5obstacle>().SetSpeed(Mg5obstacleSpeed);
+                GameObject new_Mg5obstacle = Instantiate(Mg5Obstacle);
+                float posX = Random.Range(-5f, 7.85f); // x 좌표를 랜덤으로 설정
+                new_Mg5obstacle.transform.position = new Vector3(posX,-5f, 0);
+                new_Mg5obstacle.GetComponent<Mg5Obstacle>().SetSpeed(Mg5ObstacleSpeed);
 
-                Destroy(new_Mg5obstacle, 4f );
+                Destroy(new_Mg5obstacle, 7f );
             }
 
             time = 0;
@@ -60,7 +60,7 @@ public class ObstacleSpawner : MonoBehaviour
     {
 
 
-        Mg5obstacleSpeed += 1.0f;
+        Mg5ObstacleSpeed += 1.0f;
 
         minTimeDiff -= 0.2f;
         maxTimeDiff -= 0.2f;
