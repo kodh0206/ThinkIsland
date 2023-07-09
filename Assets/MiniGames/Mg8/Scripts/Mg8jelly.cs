62c9c24bfb9f79c0ca783c5fc0f6ac45f8779ec2
heinheg <80655554+heinheg@users.noreply.github.com>
2023-07-09 21:16:25 +0900

    Mg8 재업

---
+++ Assets/MiniGames/Mg8/Scripts/Mg8jelly.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mg8jelly : MonoBehaviour
+{
+    [SerializeField]
+    private float jellySpeed = 5.0f;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * jellySpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //MiniGameManager.Instance.AddJelly();
+            Destroy(gameObject);
+        }
+
+        if (other.gameObject.tag == "Obstacle")
+        {
+            
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetSpeed(float speed)
+    {
+        jellySpeed = speed;
+    }
+}
