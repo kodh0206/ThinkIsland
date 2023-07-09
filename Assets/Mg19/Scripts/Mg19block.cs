e3feacb69ffd17f2e1cb443f9b9f7f4c638c3542
heinheg <80655554+heinheg@users.noreply.github.com>
2023-07-09 17:38:17 +0900

    Mg19리뉴얼

--- Assets/Mg19/Scripts/Mg19block.cs
+++ Assets/Mg19/Scripts/Mg19block.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Mg19block : MonoBehaviour
 {
+    public Animator animator;
+
     public GameObject breakEffect; // �μ��� �� ����� ��ƼŬ ȿ��
 
     public float moveSpeed = 5f; // �Ʒ��� �����̴� �ӵ�
 
     private void Update()
     {
-        // �Ʒ��� ������ ����
+        animator = GetComponent<Animator>();
+        
         transform.Translate(Vector2.down * moveSpeed * Time.deltaTime);
     }
 
@@ -19,7 +22,9 @@ public class Mg19block : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {   //AudioManager.Instance.BreakPlatform();
-            BreakObject();
+            animator.SetBool("PlayerPush", true); // 애니메이션 실행
+            Destroy(gameObject, animator.GetCurrentAnimatorStateInfo(0).length); // 애니메이션 재생 시간이 지난 후 블록 삭제
+            
         }
 
         if (collision.gameObject.CompareTag("Ground"))
