108c0bb2234daf39c712c7c4b37b1e6b87c3b19e
ajeong <ajung7038@naver.com>
2023-07-10 22:39:03 +0900

    BetaScene : 상점창 구현 및 동시 터치 문제 해결

---
+++ Assets/Scripts/BetaSceneButtonClick/ShopButtonClick.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopButtonClick : MonoBehaviour
+{
+
+    public GameObject sngCanvas;
+    public MonoBehaviour mobileTouchScript;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ShopCloseButtonClick()
+    {
+        gameObject.SetActive(false);
+        sngCanvas.SetActive(true);
+        mobileTouchScript.enabled = true;
+        Debug.Log("이제 다시 터치 됨.");
+    }
+}
