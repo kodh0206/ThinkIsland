5e3de378bd556583ce478209e9db79862d2889b2
ajeong <ajung7038@naver.com>
2023-07-23 03:16:57 +0900

    BetaScene : Achievement Canvas 버튼 구현

--- Assets/Scripts/BetaSceneButtonClick/AchievementButtonClick.cs
+++ Assets/Scripts/BetaSceneButtonClick/AchievementButtonClick.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AchievementButtonClick : MonoBehaviour
 {
 
+    public GameObject farmAchievementCanvas;
+    public GameObject animalAchievementCanvas;
     public GameObject sngCanvas;
     public MonoBehaviour mobileTouchScript;
     void Start()
@@ -17,10 +19,35 @@ public class AchievementButtonClick : MonoBehaviour
         
     }
 
-    public void AchievementCloseButtonClick()
+    public void FarmAchievementCloseButtonClick()
     {
         gameObject.SetActive(false);
+        farmAchievementCanvas.SetActive(true); // 초기 세팅
+        animalAchievementCanvas.SetActive(false);
         sngCanvas.SetActive(true);
         mobileTouchScript.enabled = true;
+    }
+
+    public void AnimalAchievementCloseButtonClick()
+    {
+        gameObject.SetActive(false);
+        farmAchievementCanvas.SetActive(true); // 초기 세팅
+        animalAchievementCanvas.SetActive(false);
+        sngCanvas.SetActive(true);
+        mobileTouchScript.enabled = true;
+    }
+
+    public void FarmAchievementButtonClick()
+    {
+        farmAchievementCanvas.SetActive(true);
+        animalAchievementCanvas.SetActive(false);
+        //mobileTouchScript.enabled = false;
+    }
+
+    public void AnimalAchievementButtonClick()
+    {
+        animalAchievementCanvas.SetActive(true);
+        farmAchievementCanvas.SetActive(false);
+        //mobileTouchScript.enabled = false;
     }
 }
