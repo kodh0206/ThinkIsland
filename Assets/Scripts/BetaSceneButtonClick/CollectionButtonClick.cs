ee10dbccc2fd210811088a762e80024efb34eefe
ajeong <ajung7038@naver.com>
2023-07-26 17:52:24 +0900

    BetaScene : 페이지 넘김 기능 구현

--- Assets/Scripts/BetaSceneButtonClick/CollectionButtonClick.cs
+++ Assets/Scripts/BetaSceneButtonClick/CollectionButtonClick.cs
@@ -8,6 +8,11 @@ public class CollectionButtonClick : MonoBehaviour
     public GameObject sngCanvas;
     public GameObject cropCollection;
     public GameObject characterCollection;
+
+    // 페이지 넘김(1p, 2p)
+    public GameObject page1Collection;
+    public GameObject page2Collection;
+
     public MonoBehaviour mobileTouchScript;
     void Start()
     {
@@ -40,5 +45,19 @@ public class CollectionButtonClick : MonoBehaviour
     {
         cropCollection.SetActive(false);
         characterCollection.SetActive(true);
+    }
+
+
+    // 페이지 넘김 기능 구현
+    public void page1CollectionButtonClick()
+    {
+        page1Collection.SetActive(true);
+        page2Collection.SetActive(false);
+    }
+
+    public void page2CollectionButtonClick()
+    {
+        page1Collection.SetActive(false);
+        page2Collection.SetActive(true);
     }
 }
