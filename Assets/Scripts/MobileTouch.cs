b9138378dccd32919beb2f4cf82087c16a8306c2
kodh0206 <david0206@naver.com>
2023-07-28 18:00:45 +0900

    모바일터치 수정

--- Assets/Scripts/MobileTouch.cs
+++ Assets/Scripts/MobileTouch.cs
@@ -4,18 +4,27 @@ using System;
 
 public class MobileTouch : MonoBehaviour
 {
-    private Vector3 minPosition = new Vector3(-32, -20, -20);
+   private Vector3 minPosition = new Vector3(-32, -20, -20);
     private Vector3 maxPosition = new Vector3(28, 30, 20);
     private Vector3 springVelocity = Vector3.zero;
     public float moveSpeed = 5f;
-    public float scrollSpeed = 2000.0f;
+    public float scrollSpeed = 0.1f; // Scroll speed for zooming
+    public float minOrthographicSize = 1f; // Min zoom
+    public float maxOrthographicSize = 20f; // Max zoom
     public float inertiaDuration = 1.0f;
     public float springStrength = 10.0f;
-    public float springDamping = 1.0f;  // 스프링 댐핑 계수
+    public float springDamping = 1.0f;
     private Vector3 moveVelocity = Vector3.zero;
     private bool isMoving = false;
     private Vector2 prePos, curPos, movePosDiff;
-
+        void Update()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+        TouchMove_Zoom();
+#else
+        MouseMove_Zoom();
+#endif
+    }
     void LateUpdate()
     {
         if (!isMoving && moveVelocity != Vector3.zero)
@@ -46,14 +55,7 @@ public class MobileTouch : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-#if UNITY_ANDROID && !UNITY_EDITOR
-        TouchMove_Zoom();
-#else
-        MouseMove_Zoom();
-#endif
-    }
+
 
     void MouseMove_Zoom()
     {
@@ -80,25 +82,48 @@ public class MobileTouch : MonoBehaviour
     {
         // Zoom handling code is omitted for brevity
 
-        if (Input.touchCount == 1)
+       if (Input.touchCount == 2)
         {
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began)
-            {
-                prePos = touch.position - touch.deltaPosition;
-                isMoving = true;
-            }
-            else if (touch.phase == TouchPhase.Moved)
-            {
-                curPos = touch.position - touch.deltaPosition;
-                movePosDiff = (Vector2)(prePos - curPos) * Time.deltaTime;
-                moveVelocity = new Vector3(movePosDiff.x, movePosDiff.y, 0) * moveSpeed;
-                prePos = touch.position - touch.deltaPosition;
-            }
-            if (touch.phase == TouchPhase.Ended)
-            {
-                isMoving = false;
-            }
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+
+            Camera.main.orthographicSize += deltaMagnitudeDiff * scrollSpeed;
+            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, minOrthographicSize, maxOrthographicSize);
+        }
+
+     
+    if (Input.touchCount == 1)
+    {
+        Touch touch = Input.GetTouch(0);
+
+        if (touch.phase == TouchPhase.Began)
+        {
+            prePos = touch.position - touch.deltaPosition;
+            isMoving = true;
+        }
+        else if (touch.phase == TouchPhase.Moved)
+        {
+            Vector3 deltaPosition = new Vector3(touch.deltaPosition.x, touch.deltaPosition.y, 0) * moveSpeed * Time.deltaTime;
+            Camera.main.transform.position -= deltaPosition;
+
+            curPos = touch.position - touch.deltaPosition;
+            movePosDiff = (Vector2)(prePos - curPos) * Time.deltaTime;
+            moveVelocity = new Vector3(movePosDiff.x, movePosDiff.y, 0) * moveSpeed;
+            prePos = touch.position - touch.deltaPosition;
+        }
+        if (touch.phase == TouchPhase.Ended)
+        {
+            isMoving = false;
         }
     }
+}
+
 }
