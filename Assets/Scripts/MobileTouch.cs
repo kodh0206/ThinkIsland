3e81644009683f83e561d52aea5d32233fc2f2f9
kodh0206 <david0206@naver.com>
2023-08-06 17:52:25 +0900

    ㅇㅇㅇ
    ㅇㅇ

--- Assets/Scripts/MobileTouch.cs
+++ Assets/Scripts/MobileTouch.cs
@@ -3,13 +3,13 @@ using System;
 
 
 public class MobileTouch : MonoBehaviour
-{
+{   private Vector3 targetPosition;
    private Vector3 minPosition = new Vector3(-32, -20, -20);
     private Vector3 maxPosition = new Vector3(28, 30, 20);
     private Vector3 springVelocity = Vector3.zero;
-    public float moveSpeed = 0.8f;
-    public float scrollSpeed = 0.1f; // Scroll speed for zooming
-    public float minOrthographicSize = 5f; // Min zoom
+    public float moveSpeed = 0.1f;
+    public float scrollSpeed = 0.01f; // Scroll speed for zooming
+    public float minOrthographicSize = 1f; // Min zoom
     public float maxOrthographicSize = 8f; // Max zoom
     public float inertiaDuration = 1.0f;
     public float springStrength = 10.0f;
@@ -18,7 +18,14 @@ public class MobileTouch : MonoBehaviour
     private Vector3 moveVelocity = Vector3.zero;
     private bool isMoving = false;
     private Vector2 prePos, curPos, movePosDiff;
-        void Update()
+    private float targetOrthoSize;
+    float smoothFactor = 0.1f;
+    public float maxDelta = 5f;
+    void Start()
+    {
+        targetPosition = Camera.main.transform.position; // 초기값 설정
+    }
+void Update()
     {
 #if UNITY_ANDROID && !UNITY_EDITOR
         TouchMove_Zoom();
@@ -84,29 +91,25 @@ public class MobileTouch : MonoBehaviour
         // Scroll handling code is omitted for brevity
     }
 
-    void TouchMove_Zoom()
+void TouchMove_Zoom()
     {
-        // Zoom handling code is omitted for brevity
+    if (Input.touchCount == 2)
+    {
+        Touch touchZero = Input.GetTouch(0);
+        Touch touchOne = Input.GetTouch(1);
 
-       if (Input.touchCount == 2)
-        {
-            Touch touchZero = Input.GetTouch(0);
-            Touch touchOne = Input.GetTouch(1);
+        Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+        Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
 
-            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
-            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+        float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+        float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
 
-            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
-            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+        float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
 
-            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
-
-            Camera.main.orthographicSize += deltaMagnitudeDiff * scrollSpeed * scalingFactor;
-            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, minOrthographicSize, maxOrthographicSize);
-        }
-
-     
-    if (Input.touchCount == 1)
+        targetOrthoSize = Camera.main.orthographicSize + deltaMagnitudeDiff * scrollSpeed;
+        targetOrthoSize = Mathf.Clamp(targetOrthoSize, minOrthographicSize, maxOrthographicSize);
+    }
+    else if (Input.touchCount == 1) // else if를 사용하여 한 손가락 터치와 두 손가락 터치를 동시에 인식하지 않도록 합니다.
     {
         Touch touch = Input.GetTouch(0);
 
@@ -117,17 +120,26 @@ public class MobileTouch : MonoBehaviour
         }
         else if (touch.phase == TouchPhase.Moved)
         {
-            Vector3 deltaPosition = new Vector3(touch.deltaPosition.x, touch.deltaPosition.y, 0) * moveSpeed * Time.deltaTime;
-            Camera.main.transform.position -= deltaPosition;
+        Vector3 deltaPosition = new Vector3(touch.deltaPosition.x, touch.deltaPosition.y, 0) * moveSpeed * Time.deltaTime;
+    
+        // 입력 필터링 적용
+        deltaPosition = Vector3.ClampMagnitude(deltaPosition, maxDelta);
+        // targetPosition 업데이트
+        targetPosition = Camera.main.transform.position - deltaPosition; 
 
-            prePos = touch.position;
-            moveVelocity = deltaPosition / Time.deltaTime; // 이동 속도를 적절하게 설정합니다.
+        Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, targetPosition, smoothFactor);
+
+        prePos = touch.position;
+        moveVelocity = deltaPosition / Time.deltaTime;
         }
-        if (touch.phase == TouchPhase.Ended)
+        else if (touch.phase == TouchPhase.Ended)
         {
             isMoving = false;
         }
     }
+
+    // 부드러운 줌 효과를 위한 코드
+    Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, targetOrthoSize, Time.deltaTime * 5f);
 }
 
 }
