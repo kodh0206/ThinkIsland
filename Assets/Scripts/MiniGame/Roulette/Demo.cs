cc59a900810c355dd61f127a2d57c1726acc423e
kodh0206 <david0206@naver.com>
2023-08-23 20:55:00 +0900

    룰렛씬 애니메이션 추가

--- Assets/Scripts/MiniGame/Roulette/Demo.cs
+++ Assets/Scripts/MiniGame/Roulette/Demo.cs
@@ -7,12 +7,15 @@ public class Demo : MonoBehaviour
 	private	Roulette	roulette;
 	[SerializeField]
 	private	Button		buttonSpin;
+	[SerializeField]
+    private UISpriteAnimation spriteAnimation;  // 애니메이션 컴포넌트 참조 추가
 
 	private void Awake()
 	{
 		   buttonSpin.onClick.AddListener(() =>
     {
         buttonSpin.interactable = false;
+		spriteAnimation.Func_PlayUIAnim();
         roulette.Spin(EndOfSpin);
     });
 	}
@@ -20,7 +23,7 @@ public class Demo : MonoBehaviour
 	private void EndOfSpin(RoulettePieceData selectedData)
 	{
 		  buttonSpin.interactable = false;
-
+		spriteAnimation.Func_StopUIAnim();
     Debug.Log($"{selectedData.index}:{selectedData.description}");
     
   
