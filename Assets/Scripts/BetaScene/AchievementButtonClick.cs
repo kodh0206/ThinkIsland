ce8dbf81a76a6ecd9ef41a33c4173bb9942aad79
ajeong <ajung7038@naver.com>
2023-08-30 18:15:44 +0900

    캐릭터 업적 관련 수정

--- Assets/Scripts/BetaScene/AchievementButtonClick.cs
+++ Assets/Scripts/BetaScene/AchievementButtonClick.cs
@@ -7,9 +7,31 @@ public class AchievementButtonClick : MonoBehaviour
 
     public GameObject sngCanvas;
     public MonoBehaviour mobileTouchScript;
+
+    int activatedCharacterCount = 0; // 활성화된 characterObjects의 수를 저장할 변수
+
     void Start()
     {
-        
+        AchievementManager achievementManager = FindObjectOfType<AchievementManager>();
+        if (achievementManager != null)
+        {
+            int[] requiredCharacterCounts = { 4, 8, 12, 16, 20 };
+            string[] achievementIds = { "28", "29", "30", "31", "32" };
+
+            for (int i = 0; i < requiredCharacterCounts.Length; i++)
+            {
+                if (GameController.Instance.unlockedMiniGames.Count >= requiredCharacterCounts[i])
+                {
+                    achievementManager.IncrementAchievement(achievementIds[i], requiredCharacterCounts[i]);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+        }
+
     }
 
     void Update()
