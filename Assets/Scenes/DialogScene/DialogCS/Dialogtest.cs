0ef04eb244f15d8bc2bb92be3c54676bb76d5c22
heinheg <80655554+heinheg@users.noreply.github.com>
2023-09-01 01:03:51 +0900

    다이얼로그 샘플

--- Assets/Scenes/DialogScene/DialogCS/Dialogtest.cs
+++ Assets/Scenes/DialogScene/DialogCS/Dialogtest.cs
@@ -14,31 +14,32 @@ public class Dialogtest : MonoBehaviour
 
     private IEnumerator Start()
     {
-        textCountdown.gameObject.SetActive(false);
+        //textCountdown.gameObject.SetActive(false);
 
         // 첫 번째 대사 분기 시작
         yield return new WaitUntil(() => dialogSystem01.UpdateDialog());
 
-        // 대사 분기 사이에 원하는 행동을 추가할 수 있다.
-        // 캐릭터를 움직이거나 아이템을 획득하는 등의.. 현재는 5-4-3-2-1 카운트 다운 실행
-        textCountdown.gameObject.SetActive(true);
-        int count = 5;
-        while (count > 0)
-        {
-            textCountdown.text = count.ToString();
-            count--;
+        //// 대사 분기 사이에 원하는 행동을 추가할 수 있다.
+        //// 캐릭터를 움직이거나 아이템을 획득하는 등의.. 현재는 5-4-3-2-1 카운트 다운 실행
+        //textCountdown.gameObject.SetActive(true);
+        //int count = 5;
+        //while (count > 0)
+        //{
+        //    textCountdown.text = count.ToString();
+        //    count--;
 
-            yield return new WaitForSeconds(1);
-        }
-        textCountdown.gameObject.SetActive(false);
+        //    yield return new WaitForSeconds(1);
+        //}
+        //textCountdown.gameObject.SetActive(false);
 
-        // 두 번째 대사 분기 시작
-        yield return new WaitUntil(() => dialogSystem02.UpdateDialog());
+        //// 두 번째 대사 분기 시작
+        //yield return new WaitUntil(() => dialogSystem02.UpdateDialog());
 
-        textCountdown.gameObject.SetActive(true);
-        textCountdown.text = "The End";
+        //textCountdown.gameObject.SetActive(true);
+        //textCountdown.text = "The End";
 
         yield return new WaitForSeconds(2);
+        //이후 씬 전환 SceneChange
 
     }
 }
