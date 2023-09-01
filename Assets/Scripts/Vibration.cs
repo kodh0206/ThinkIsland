f4491d4c3ffd90133b3a520ec88ee89413053478
kodh0206 <david0206@naver.com>
2023-09-01 18:00:39 +0900

    설정 진동추가
    ㄹㄹ

--- Assets/Scripts/Vibration.cs
+++ Assets/Scripts/Vibration.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Vibration: MonoBehaviour
 {   
-    private static Vibration _instance;
+     private static Vibration _instance;
     public static Vibration Instance { get { return _instance; } }
-    public bool isVibrate;
+    public bool isVibrate = true;
+
     private void Awake()
     {
         if (_instance != null && _instance != this)
@@ -17,12 +18,44 @@ public class Vibration: MonoBehaviour
 
         _instance = this;
         DontDestroyOnLoad(gameObject);
+
+        LoadState();
     }
 
     public void Vibrate()
     {
-        #if UNITY_ANDROID || UNITY_IOS
+#if UNITY_ANDROID || UNITY_IOS
+        if (isVibrate)
+        {
             Handheld.Vibrate();
-        #endif
+        }
+#endif
+    }
+
+    public void SaveState()
+    {
+        PlayerPrefs.SetInt("IsVibrate", isVibrate ? 1 : 0);
+        PlayerPrefs.Save();
+    }
+
+    public void LoadState()
+    {
+        if (PlayerPrefs.HasKey("IsVibrate"))
+        {
+            isVibrate = PlayerPrefs.GetInt("IsVibrate") == 1 ? true : false;
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveState();
+    }
+
+    private void OnApplicationPause(bool pauseStatus)
+    {
+        if (pauseStatus)
+        {
+            SaveState();
+        }
     }
 }
